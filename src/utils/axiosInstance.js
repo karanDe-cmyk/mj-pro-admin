2693c9d82eb2d6341cf70579443522ab63568acf
karan <kk899168@gmail.com>
2025-08-12 17:28:15 +0530

    starline

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -4,9 +4,9 @@ import "react-toastify/dist/ReactToastify.css";
 
 // ✅ Use runtime-configured API URL
 const instance = axios.create({
-  baseURL :'https://maya-api.kglame.com'
+  // baseURL :'https://maya-api.kglame.com'
   // baseURL: "https://matkabar-api.kglame.com"
-  // baseURL: window.ENV?.API_URL,
+  baseURL: window.ENV?.API_URL,
   // baseURL: "http://localhost:5002"
 });
 
