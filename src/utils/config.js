62dcd36924a706a9313fee070fb3faff304bb5f3
karan <kk899168@gmail.com>
2025-08-21 14:13:44 +0530

    config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
-// export const apiUrl = "https://maya-api.kglame.com"
+export const apiUrl = "https://maya-api.kglame.com"
 // export const dummyUrl ="https://jsonplaceholder.typicode.com/posts"
-// export const apiUrl = "http://localhost:5001";
-export const apiUrl = window.ENV.API_URL;
+// export const apiUrl = "http://localhost:5002";
+// export const apiUrl = window.ENV.API_URL;
 // export const apiUrl = window.ENV?.API_URL;
 
 
