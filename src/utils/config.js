6346eee79219366cfd3cf73d75d4d41a781d04ad
karan <kk899168@gmail.com>
2025-09-02 20:47:10 +0530

    telegram

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,6 @@
-export const apiUrl = "https://maharanimatka-api.kglame.com"
-// export const dummyUrl ="https://jsonplaceholder.typicode.com/posts"
-// export const apiUrl = "http://localhost:5002";
+// export const apiUrl = "https://maharanimatka-api.kglame.com"
+export const apiUrl = "http://localhost:5002";
 // export const apiUrl = window.ENV.API_URL;
-// export const apiUrl = window.ENV?.API_URL;
 
 
 
