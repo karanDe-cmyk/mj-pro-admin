4305a578c259b7ff83402f993cce648fe51aa434
karan <kk899168@gmail.com>
2025-09-06 17:02:04 +0530

    axios

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -4,8 +4,8 @@ import "react-toastify/dist/ReactToastify.css";
 
 // ✅ Use runtime-configured API URL
 const instance = axios.create({
+  // baseURL :'https://phoenix365-api.kglame.com'
   baseURL :'https://mjprogames-api.kglame.com'
-  // baseURL :'https://bfbb2062000a.ngrok-free.app'
   // baseURL: "https://matkabar-api.kglame.com"
   // baseURL: window.ENV?.API_URL,
   // baseURL: "http://localhost:5002"
