701d82b2dd4df972e26439e2be9be08cc3dab17f
Karan Kumar <kk899168@gmail.com>
2025-09-07 15:53:16 +0530

    axis

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,4 @@
 // export const apiUrl = "https://maharanimatka-api.kglame.com"
-// export const dummyUrl ="https://jsonplaceholder.typicode.com/posts"
 export const apiUrl = "http://localhost:5002";
 // export const apiUrl = window.ENV.API_URL;
 // export const apiUrl = window.ENV?.API_URL;
