cb0abdbc6c7f8bb8d320a7c598b60da133c5a159
karan <kk899168@gmail.com>
2025-09-14 01:50:19 +0530

    config

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,6 +4,3 @@ export const apiUrl = "https://mjprogames-api.kglame.com"
 // export const apiUrl = "http://localhost:5002";
 // export const apiUrl = window.ENV.API_URL;
 // export const apiUrl = window.ENV?.API_URL;
-
-
-
