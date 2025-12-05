caaabb13a6e9d124a5ed58451ca8d2bc08b8cf56
karan <kk899168@gmail.com>
2025-12-05 11:03:27 +0530

    url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -4,8 +4,8 @@ import "react-toastify/dist/ReactToastify.css";
 
 // ✅ Use runtime-configured API URL
 const instance = axios.create({
-  // baseURL :'https://maharanimatka-api.kglame.com'
-  baseURL: "http://localhost:5002"
+  baseURL :'https://maharanimatka-api.kglame.com'
+  // baseURL: "http://localhost:5002"
   // baseURL: window.ENV?.API_URL,
 });
 
