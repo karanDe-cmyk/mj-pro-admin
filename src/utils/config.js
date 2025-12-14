2e2243862a9155c344b1bc2ba341bd998024cad5
karan <kk899168@gmail.com>
2025-12-14 20:54:47 +0530

    jackpot

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
-// export const apiUrl = "https://maharanimatka-api.kglame.com"
-export const apiUrl = "http://localhost:5002";
+export const apiUrl = "https://maharanimatka-api.kglame.com"
+// export const apiUrl = "http://localhost:5002";
 // export const apiUrl = window.ENV.API_URL;
 
 
