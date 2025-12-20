c9a3672aa327b05f6205d8faea85ddcd4927503b
karan <kk899168@gmail.com>
2025-12-20 10:54:55 +0530

    init

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -153,7 +153,7 @@ const Login = () => {
                 />
               </div>
               <Title level={4} className="mt-2.5">
-                Admin Panel
+                Mjpro Admin Panel
               </Title>
             </div>
 
