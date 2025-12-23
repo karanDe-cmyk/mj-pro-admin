389985b46ed5a4ea1f2c5bcdc3f5f0d1ba427b50
karan <kk899168@gmail.com>
2025-12-23 19:48:30 +0530

    init

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 // ✅ Use runtime-configured API URL
 const instance = axios.create({
-  baseURL :'https://demo-api.phoenixappapi.com'
+  baseURL :'https://bazigar-api.phoenixappapi.com'
   // baseURL: "http://localhost:5002"
 });
 
