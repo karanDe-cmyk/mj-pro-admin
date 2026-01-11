f36d0af569c3d564e49dd6db5dc50881d447d0e7
Macco Tech <info.maccotech@gmail.com>
2026-01-11 22:50:59 +0530

    fdbfdsf

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-export const apiUrl = "https://madhur567-api.kglame.com"
+export const apiUrl = "https://madhur-api.maccotech.in"
 // export const dummyUrl ="https://jsonplaceholder.typicode.com/posts"
 // export const apiUrl = "http://localhost:5002";
 // export const apiUrl = window.ENV.API_URL;
