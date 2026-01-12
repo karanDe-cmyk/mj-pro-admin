998bf225757fb905c519db79c5ce8c3e0852e913
Macco Tech <info.maccotech@gmail.com>
2026-01-12 21:04:22 +0530

    ndsjdsf

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 // ✅ Use runtime-configured API URL
 const instance = axios.create({
-  baseURL :'https://madhur-api.maccotech.in'
+  baseURL :'https://tara-api.tara365matka.space'
   // baseURL: "https://maya-api.kglame.com"
   // baseURL: window.ENV?.API_URL,
   // baseURL: "http://localhost:5002"
