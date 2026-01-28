e6b27bce8078541936a5fd9be54fbd0177e1fcf9
karan <kk899168@gmail.com>
2026-01-28 11:38:47 +0530

    init

--- src/firebase.js
+++ src/firebase.js
@@ -4,11 +4,11 @@ import { getMessaging } from "firebase/messaging"
 const firebaseConfig = {
     apiKey: "AIzaSyBPyeOhmFRnpi8C6WdmC1KPNm6GAIpUiLw",
     authDomain: "matkabar-1ec29.firebaseapp.com",
+    databaseURL: "https://matkabar-1ec29-default-rtdb.firebaseio.com",
     projectId: "matkabar-1ec29",
     storageBucket: "matkabar-1ec29.firebasestorage.app",
     messagingSenderId: "1076308028320",
-    appId: "1:1076308028320:web:aaf9551080077cb6f452c5",
-    measurementId: "G-EVVVDGB5CC",
+    appId: "1:1076308028320:web:c71d14b52a1409f3f452c5",
     databaseURL: "https://matkabar-1ec29-default-rtdb.firebaseio.com",
 };
 
