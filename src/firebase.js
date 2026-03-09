00f51ff21042812ad6d58975e3b0f296750ffac0
karan <kk899168@gmail.com>
2026-03-09 22:24:05 +0530

    fix: update Firebase configuration values

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging"
 // const firebaseConfig = window.ENV?.FIREBASE_CONFIG;
 const firebaseConfig = {
-    apiKey: "AIzaSyBPyeOhmFRnpi8C6WdmC1KPNm6GAIpUiLw",
+    apiKey: "AIzaSyB1WgqxxEeB_wZ6KQkMBM8QH8S8h5GhgNg",
     authDomain: "matkabar-1ec29.firebaseapp.com",
+    databaseURL: "https://matkabar-1ec29-default-rtdb.firebaseio.com",
     projectId: "matkabar-1ec29",
     storageBucket: "matkabar-1ec29.firebasestorage.app",
     messagingSenderId: "1076308028320",
-    appId: "1:1076308028320:web:aaf9551080077cb6f452c5",
+    appId: "1:1076308028320:web:b7a5e262840d87d4f452c5",
     measurementId: "G-EVVVDGB5CC",
-    databaseURL: "https://matkabar-1ec29-default-rtdb.firebaseio.com",
 };
 
 export const app = initializeApp(firebaseConfig);
