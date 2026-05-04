81f306f87f461c8dd02b57fcbc0261977ed7dbb5
karan <kk899168@gmail.com>
2026-05-04 12:22:21 +0530

    api change

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 // ✅ Use runtime-configured API URL
 const instance = axios.create({
-  baseURL :'https://lotus-api.phoenixappapi.com'
+  baseURL :'https://demo-api.skylinkapi.online'
   // baseURL: "http://localhost:5002"
 });
 
